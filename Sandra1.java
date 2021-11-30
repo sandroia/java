521e27067cec24948bd07c7b78c0d3a2df97105f
sandroia <91122485+sandroia@users.noreply.github.com>
2021-11-30 14:56:01 +0100

    Update Sandra1.java

--- Sandra1.java
+++ Sandra1.java
@@ -8,11 +8,11 @@ int n4;
 int soma;
 Scanner a = new Scanner(System.in);
 System.out.println("Digite os numeros");
-int n1 = a.nextInt();
-int n2 = a.nextInt();
-int n3 = a.nextInt();
-int n4 = a.nextInt();
-int soma = n1 + n2 + n3 + n4;
+ n1 = a.nextInt();
+ n2 = a.nextInt();
+ n3 = a.nextInt();
+ n4 = a.nextInt();
+ soma = n1 + n2 + n3 + n4;
 System.out.println("A soma desses numeros e="+soma);
 }
 }
