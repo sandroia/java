41d8764a0161a7ccd2803eacbadb2f5ee616f4bf
sandroia <91122485+sandroia@users.noreply.github.com>
2021-11-30 16:49:50 +0100

    Update Sandra16.java

--- Sandra16.java
+++ Sandra16.java
@@ -3,20 +3,20 @@ public class Sandra16{
 public static void main (String[] args){
 int a = 12;
 int b = 3;
-int c = 1,760;
+int c = 1.760;
 int poleg;
 int pe;
 int jard;
 float milhaa;
 Scanner b = new Scanner(System.in);
 System.out.println("Digite os valores em pes");
-int pe = b.nextInt();
+ pe = b.nextInt();
 System.out.println("Converta polegadas");
-int poleg = pe * a;
+ poleg = pe * a;
 System.out.println("Converta jardas");
-int jard = poleg * b;
+ jard = poleg * b;
 System.out.println("Converta milha"); 
-float milhaa = jard * c;
+ milhaa = jard * c;
 System.out.println(" Em polegadas e="+poleg);
 System.out.println(" Em jardas e="+jard);
 System.out.println(" Em milha e="+milhaa);
