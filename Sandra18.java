869a9be20ed31f08b43c911776131c136262bdea
sandroia <91122485+sandroia@users.noreply.github.com>
2021-11-30 16:53:55 +0100

    Update Sandra18.java

--- Sandra18.java
+++ Sandra18.java
@@ -5,9 +5,9 @@ float precp;
 float novoprec;
 Scanner n = new Scanner(System.in);
 System.out.println("Digite o preço do produto");
-float precp = n.nextFloat();
+ precp = n.nextFloat();
 System.out.println("Calcule o novo preço");
-float novoprec = precp - 10%;
+ novoprec = precp - (precp*0.10);
 System.out.println("O novo preço e="+novoprec);
 }
 }
