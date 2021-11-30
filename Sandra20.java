c00d5d49ff361969dc6bbab41fbc13db49938a45
sandroia <91122485+sandroia@users.noreply.github.com>
2021-11-30 17:00:44 +0100

    Update Sandra20.java

--- Sandra20.java
+++ Sandra20.java
@@ -6,11 +6,11 @@ float precc;
 float quantc;
 Scanner n = new Scanner(System.in);
 System.out.println("Digite o custo de um espetaculo teatral");
-float custt = n.nextFloat();
+ custt = n.nextFloat();
 System.out.println("Digite o preco do convite");
-float precc = n.nextFloat();
+ precc = n.nextFloat();
 System.out.println("Calcule a quantidade de convites");
-float quantc = custt /precc; 
+ quantc = custt /precc; 
 System.out.println("A quantidade de covite que devem ser vendidos e="+quantc);
 }
 }
